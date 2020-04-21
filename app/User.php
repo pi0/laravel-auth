e514d2819876dbfe61969ff9a31d2b24bf506c38
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2020-04-21 11:17:17 -0300

    feat(user model): use passport `HasApiTokens`

--- app/User.php
+++ app/User.php
@@ -5,7 +5,7 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Laravel\Passport\HasApiTokens;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable implements JWTSubject
