6a9aa603196106a04c3143487c2cab700e574941
Pooya Parsa <pyapar@gmail.com>
2020-12-15 22:28:48 +0100

    seed database with test user

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,10 @@
 <?php
 
+
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        DB::table('users')->insert([
+            'name' => 'test user',
+            'email' => 'test@test.com',
+            'password' => Hash::make('12345678'),
+        ]);
     }
 }
